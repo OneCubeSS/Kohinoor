66ef56516df0036c291f8451d4ee76596a4c0fc7
Radhika Kotthapalli <radhika@reachhcs.com>
2020-10-23 21:11:19 +0530

    commiting changes

--- js/custom.js
+++ js/custom.js
@@ -43,4 +43,20 @@ $(document).ready(function() {
         }
       });
     });
+    $(function () {
+      function toggleOpen(e) {
+        $(e.target)
+          .prev('.card-header')
+          .find(".expand-icon")
+          .text("remove_circle");
+      }
+      function toggleClose(e) {
+        $(e.target)
+          .prev('.card-header')
+          .find(".expand-icon")
+          .text("add_circle");
+      }
+      $('.panel-group').on('hidden.bs.collapse', toggleClose);
+      $('.panel-group').on('shown.bs.collapse', toggleOpen);
+    });
 });    
